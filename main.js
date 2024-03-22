7599c1f8fbc84a258b2fd702ff89e74698ce93b5
Eva Garcia <evagarciaclemente96@gmail.com>
2024-03-22 12:44:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,116 @@
+function resta (a, b) {
+  return (a - b)
+}
+console.log(resta(6, 2));
+
+
+
+
+
+
+function nota (num) {
+  switch (true) {
+    case (num <=4) :
+      return ('Insuficiente')
+      
+    case (num >4 && num <= 6) :
+      return ('Suficiente')
+      
+    case (num >6 && num <= 8) :
+      return ('Notable')
+      
+    case (num >8 && num <= 10) :
+      return ('Sobresaliente')
+      
+    default:
+      return ' no existe esa nota'
+  }
+}
+console.log(nota(7));
+
+
+
+
+
+
+
+function duplicaNumero (num) {
+  if (typeof num === 'number') {
+    return num * 2
+  } else if (typeof num != "number") {
+    return 'Debo ser ejecutada con un número'
+}}
+console.log(duplicaNumero(6));
+
+
+
+
+
+
+
+function caracterInicial (word) {
+ switch (true) {
+  case !word || word.length == 0:
+    return 'Debo ser ejecutada con un string no vacío'
+  case typeof word === 'string':
+    return word[0]
+  case typeof word != 'string':
+    return 'Debo ser ejecutada con un string'
+ 
+  default:
+    return 'Algo no está funcionando...'
+ }
+}
+console.log(caracterInicial('Eva'))
+
+
+
+
+
+
+
+function ultimoCaracter (word) {
+  switch (true) {
+    case !word || word.length == 0:
+      return 'Debo ser ejecutada con un string no vacío'
+    case typeof word === 'string':
+      return word[word.length - 1]
+    case typeof word != 'string':
+      return 'Debo ser ejecutada con un string'
+   
+   default:
+     return 'Algo no está funcionando...'
+  }
+ }
+ console.log(ultimoCaracter('Javier'))
+
+
+
+
+
+
+
+
+ function cuentaCaracteres (string) {
+    
+    if (typeof string == 'string') {
+      return string.length
+    } else if (typeof string != 'string'){
+      return 'Debo ser ejecutada con un string'
+    }
+ }
+
+ console.log(cuentaCaracteres('Girasol'))
+
+
+
+
+
+
+
+
+
+
+function esPalindromo () {
+  
+}
